1c7ea19ba40ebbe16df80b7c9a8ee3f1ae0f102c
Szabolcs Veer <futuremove@gmail.com>
2018-09-22 14:26:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function addElementToEndOfArray(x,one){
   return [...x,one];
 }
 function destructivelyAddElementToEndOfArray(x,one){
-  return .push();
+  return one.push();
 }
 var n = [1, 2, 3];
 function accessElementInArray(n,i){
