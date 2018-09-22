6708dcf7e9d5995aba4d3b08d572c857f6ddd6da
Szabolcs Veer <futuremove@gmail.com>
2018-09-22 12:58:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,36 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
+
+var x = ["foo"];
+
+const one = [1];
+function addElementToBeginningOfArray(x, one){
+  return [one, ...x];
+}
+
+function destructivelyAddElementToBeginningOfArray(x,one){
+  return x.unshift(one);
+  
+}
+function addElementToEndOfArray(x,one){
+  return [...x,1];
+}
+function destructivelyAddElementToEndOfArray(x,one){
+  return x.push("foo");
+}
+var n = [1, 2, 3];
+function accessElementInArray(n,i){
+  return n[2];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(x){
+  return x.shift(x);
+}
+function removeElementFromBeginningOfArray(x){
+  return x.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray(x){
+  return x.pop(1);
+  }
+function removeElementFromEndOfArray(x){
+  return x.slice(0);
+}
